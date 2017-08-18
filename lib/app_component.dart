d44a52369a1c01c94618080c4b8a51d74eb9397c
NickWu <wujunao19950715@gmail.com>
2017-08-18 13:19:39 +0100

    added create new team component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, NickWu. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:Teamoji_tutorial/src/create_team/create_team.dart';
 import 'package:Teamoji_tutorial/src/welcome/welcome_page.dart';
 import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
@@ -13,7 +14,7 @@ import 'package:angular_components/angular_components.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, WelcomePageComponent],
+  directives: const [materialDirectives, WelcomePageComponent, CreateTeamComponent],
   providers: const [materialProviders],
 )
 class AppComponent {
---
+++ lib/src/create_team/create_team.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'dart:html';
+import 'package:angular2/core.dart';
+import 'package:angular_components/angular_components.dart';
+
+@Component(
+  selector: 'create-team',
+  templateUrl: 'create_team.html',
+  styleUrls: const ['create_team.css'],
+  directives: const [
+    MaterialButtonComponent,
+    MaterialInputComponent,
+    NgModel,
+  ],
+)
+class CreateTeamComponent {
+
+  String get createTeamPromptHeader => 'Feeling a little lonely?';
+
+  String get createTeamPromptContent => 'Let\'s make a new team!';
+
+  String get pageIcon => '\u{1F914}';
+
+  String get createTeamButtonMessage => 'create team';
+
+  String newTeamName = null;
+
+  Future<Null> create() async {
+    print('You want to create a new team called: $newTeamName');
+    newTeamName = '';
+  }
+}
